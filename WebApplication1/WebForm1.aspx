459460a260e54aaad8d33aad5d08736155c18b69
Korice <hung20020307@gmail.com>
2024-05-01 22:35:44 +0800

    first commit

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style>
+        #wrapper{
+            width:600px;
+            margin:10px auto;
+            border:1px solid #f00;
+            padding:10px;
+            text-align:center;
+            font-size:14px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="wrapper">
+            <img src="jpg/TREE.jpg" />
+            <br />
+            <asp:Label ID="Date" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Label ID="Welcome" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Label ID="GiftShow" runat="server" Text=""></asp:Label>
+            <br />
+            <asp:Button ID="EndShow" runat="server" Text="抽獎" OnClick="EndShow_Click" />
+            <br />
+            <asp:Image ID="Image1" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {        
+        
+        int RandomNumber;
+        Random oner = new Random();
+        String[] Gift = new string[] { "筆記型電腦", "隨身碟", "智慧型手機", "馬克杯", "平板電腦", "面紙" };
+        String[] GiftName = new string[] { "NoteBook", "FlashDrive", "Iphone", "Cup", "Tabket", "Paper" };
+
+        protected void Page_Load(object sender, EventArgs e)
+        
+        {
+
+            DateTime ChrisDate = new DateTime(2024, 12, 25);
+            TimeSpan Diff = ChrisDate - DateTime.Now;
+            Date.Text = "距離聖誕節還剩:" + Diff.Days + "天";
+
+            Welcome.Text = "以下是我們準備的禮物:";
+
+            for (int i = 0; i < Gift.Length; i++)
+            {
+                if (i != Gift.Length - 1)
+                    Welcome.Text += Gift[i] + "、";
+                else
+                    Welcome.Text += Gift[i] + "。";
+            }
+
+        }
+
+        protected void EndShow_Click(object sender, EventArgs e)
+        {
+
+            RandomNumber = oner.Next(0, 6);
+            GiftShow.Text = "你抽到的是:" + Gift[RandomNumber] + "<br>";
+            Image1.ImageUrl = "jpg/" + GiftName[RandomNumber] + ".jpg";
+
+        }
+    }
+}
